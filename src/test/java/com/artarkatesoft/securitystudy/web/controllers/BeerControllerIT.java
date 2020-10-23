1ee49f27642226403214acb9d0b1532d603dfc14
Art <d.art.shishkin@gmail.com>
2020-10-23 11:55:36 +0300

    21 Testing HTTP Basic Auth

--- src/test/java/com/artarkatesoft/securitystudy/web/controllers/BeerControllerIT.java
+++ src/test/java/com/artarkatesoft/securitystudy/web/controllers/BeerControllerIT.java
@@ -21,23 +21,28 @@ import com.artarkatesoft.securitystudy.domain.Beer;
 import com.artarkatesoft.securitystudy.repositories.BeerInventoryRepository;
 import com.artarkatesoft.securitystudy.repositories.BeerRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 import java.util.UUID;
 
 import static org.mockito.Mockito.verifyNoInteractions;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -57,7 +62,7 @@ class BeerControllerIT {
     UUID uuid;
     Beer beer;
 
-//    @Autowired
+    //    @Autowired
     MockMvc mockMvc;
 
     Page<Beer> beers;
@@ -89,4 +94,50 @@ class BeerControllerIT {
         verifyNoInteractions(beerRepository);
     }
 
+    @Test
+    @DisplayName("Entering WRONG username and password should produce 401 Unauthorized status code")
+    void findBeersWithHttpBasic_wrongCredentials() throws Exception {
+        mockMvc.perform(
+                get("/beers/find")
+                        .with(httpBasic("userFake", "passwordFake")))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @DisplayName("Entering CORRECT username and password should allow access")
+    void findBeersWithHttpBasic_ok() throws Exception {
+        mockMvc.perform(
+                get("/beers/find")
+                        .with(httpBasic("art", "123")))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/findBeers"))
+                .andExpect(model().attributeExists("beer"));
+    }
+
+    @Test
+    @DisplayName("Entering CORRECT credentials through Http Header")
+    void findBeersWithHttpBasic_okUsingHeader() throws Exception {
+        String credentials = "art:123";
+        String credentials64 = Base64.getEncoder().encodeToString(credentials.getBytes());
+
+        mockMvc.perform(
+                get("/beers/find")
+                        .header(HttpHeaders.AUTHORIZATION, "Basic " + credentials64))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/findBeers"))
+                .andExpect(model().attributeExists("beer"));
+    }
+
+    @Test
+    @DisplayName("Entering CORRECT credentials and using HttpHeaders.encodeBasicAuth")
+    void findBeersWithHttpBasic_okUsingHeaderAndSpringFeatures() throws Exception {
+        mockMvc.perform(
+                get("/beers/find")
+                        .header(
+                                HttpHeaders.AUTHORIZATION,
+                                "Basic " + HttpHeaders.encodeBasicAuth("art", "123", StandardCharsets.UTF_8)))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/findBeers"))
+                .andExpect(model().attributeExists("beer"));
+    }
 }
