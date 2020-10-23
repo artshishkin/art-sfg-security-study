c0c379f415a76e30fbd1534e5feb6de472ffa246
Art <d.art.shishkin@gmail.com>
2020-10-23 17:20:57 +0300

    25 Http Method Matching

---
+++ src/test/java/com/artarkatesoft/securitystudy/web/controllers/api/BeerRestControllerIT.java
@@ -0,0 +1,39 @@
+package com.artarkatesoft.securitystudy.web.controllers.api;
+
+import com.artarkatesoft.securitystudy.services.BeerService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BeerRestController.class)
+class BeerRestControllerIT {
+
+    @MockBean
+    BeerService beerService;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void findBeers() throws Exception {
+        //when
+        mockMvc.perform(get("/api/v1/beer"))
+
+                //then
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void findById() throws Exception {
+        //when
+        mockMvc.perform(get("/api/v1/beer/493410b3-dd0b-4b78-97bf-289f50f6e74f"))
+
+                //then
+                .andExpect(status().isOk());
+    }
+}
