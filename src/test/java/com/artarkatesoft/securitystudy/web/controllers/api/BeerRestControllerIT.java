d19e6ef0a683cbd01d9d8afb2af9cab624e4ade6
Art <d.art.shishkin@gmail.com>
2020-10-24 15:45:51 +0300

    42 Delete Beer by ID MockMVC Test

--- src/test/java/com/artarkatesoft/securitystudy/web/controllers/api/BeerRestControllerIT.java
+++ src/test/java/com/artarkatesoft/securitystudy/web/controllers/api/BeerRestControllerIT.java
@@ -1,12 +1,14 @@
 package com.artarkatesoft.securitystudy.web.controllers.api;
 
 import com.artarkatesoft.securitystudy.services.BeerService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -44,5 +46,17 @@ class BeerRestControllerIT {
 
                 //then
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("When delete beer by ID with WRONG credentials (may be met in LEGACY systems)")
+    void deleteById() throws Exception {
+        //when
+        mockMvc.perform(
+                delete("/api/v1/beer/493410b3-dd0b-4b78-97bf-289f50f6e74f")
+                        .header("Api-Key", "art")
+                        .header("Api-Secret", "123"))
+                //then
+                .andExpect(status().is4xxClientError());
     }
 }
