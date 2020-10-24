c70793bbc75ef78956e78a759c82755e424a698e
Art <d.art.shishkin@gmail.com>
2020-10-24 16:48:48 +0300

    44 Spring Security Configuration [skip ci]

--- src/main/java/com/artarkatesoft/securitystudy/config/SecurityConfig.java
+++ src/main/java/com/artarkatesoft/securitystudy/config/SecurityConfig.java
@@ -1,20 +1,35 @@
 package com.artarkatesoft.securitystudy.config;
 
 import com.artarkatesoft.securitystudy.security.ArtPasswordEncoderFactories;
+import com.artarkatesoft.securitystudy.security.RestHeaderAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    public RestHeaderAuthFilter restHeaderAuthFilter(AuthenticationManager manager) {
+        RestHeaderAuthFilter authFilter = new RestHeaderAuthFilter(new AntPathRequestMatcher("/api/**"));
+        authFilter.setAuthenticationManager(manager);
+        return authFilter;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        http.addFilterBefore(restHeaderAuthFilter(this.authenticationManager()),
+                UsernamePasswordAuthenticationFilter.class);
+
         http
                 .authorizeRequests(authorizedUrl ->
                         authorizedUrl
