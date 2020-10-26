f5519f7ea341a5f8c7abd7807cca2f0ed72e293e
Art <d.art.shishkin@gmail.com>
2020-10-26 10:01:28 +0200

    54 User Details Service. #2

---
+++ src/main/java/com/artarkatesoft/securitystudy/security/JpaUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.artarkatesoft.securitystudy.security;
+
+import com.artarkatesoft.securitystudy.repositories.security.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JpaUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User " + username + " not found"));
+    }
+}
