2c1aa95b3eacceed8f633b0d6ced543337150b75
Art <d.art.shishkin@gmail.com>
2020-10-28 17:08:09 +0200

    Assignment 11: Assignment - Update to Use Custom Auth Annotations

--- src/main/java/com/artarkatesoft/securitystudy/web/controllers/BreweryController.java
+++ src/main/java/com/artarkatesoft/securitystudy/web/controllers/BreweryController.java
@@ -18,9 +18,9 @@
 package com.artarkatesoft.securitystudy.web.controllers;
 
 import com.artarkatesoft.securitystudy.domain.Brewery;
+import com.artarkatesoft.securitystudy.security.perms.BreweryReadPermission;
 import com.artarkatesoft.securitystudy.services.BreweryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,13 +36,13 @@ public class BreweryController {
 
     private final BreweryService breweryService;
 
-    @PreAuthorize("hasAuthority('brewery.read')")
+    @BreweryReadPermission
     @GetMapping({"/breweries", "/breweries/index", "/breweries/index.html", "/breweries.html"})
     public String listBreweries(Model model) {
         model.addAttribute("breweries", breweryService.getAllBreweries());
         return "breweries/index";
     }
-    @PreAuthorize("hasAuthority('brewery.read')")
+    @BreweryReadPermission
     @GetMapping("/api/v1/breweries")
     public @ResponseBody
     List<Brewery> getBreweriesJson(){
