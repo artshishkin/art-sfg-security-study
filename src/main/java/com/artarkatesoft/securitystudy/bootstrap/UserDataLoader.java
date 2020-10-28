5fff17cd375d3e9fc33387768e3656c692266ba5
Art <d.art.shishkin@gmail.com>
2020-10-28 09:12:57 +0200

    fixed bug when persisting roles, authorities and users. closes #5 [skip ci]

--- src/main/java/com/artarkatesoft/securitystudy/bootstrap/UserDataLoader.java
+++ src/main/java/com/artarkatesoft/securitystudy/bootstrap/UserDataLoader.java
@@ -37,7 +37,7 @@ public class UserDataLoader implements CommandLineRunner {
         //Beer auth
         List<Authority> authorities = Stream.of("beer.create", "beer.read", "beer.update", "beer.delete")
                 .map(permission -> Authority.builder().authority(permission).build())
-                .map(authorityRepository::save)
+//                .map(authorityRepository::save)
                 .collect(Collectors.toList());
 
         Authority createBeer = authorities.get(0);
@@ -52,15 +52,22 @@ public class UserDataLoader implements CommandLineRunner {
 //                Role.builder().name("CUSTOMER").authority(readBeer).build());
 //        Role userRole = roleRepository.save(
 //                Role.builder().name("USER").authority(readBeer).build());
-        Role adminRole = roleRepository.save(Role.builder().name("ADMIN").build());
-        Role customerRole = roleRepository.save(Role.builder().name("CUSTOMER").build());
-        Role userRole = roleRepository.save(Role.builder().name("USER").build());
+        Role adminRole = Role.builder().name("ADMIN").authorities(Set.of(createBeer, readBeer, updateBeer, deleteBeer)).build();
+        Role customerRole = Role.builder().name("CUSTOMER").authority(readBeer).build();
+        Role userRole = Role.builder().name("USER").authority(readBeer).build();
 
-        adminRole.setAuthorities(Set.of(createBeer, readBeer, updateBeer, deleteBeer));
-        customerRole.setAuthorities(Set.of(readBeer));
-        userRole.setAuthorities(Set.of(readBeer));
+//        Role adminRole = roleRepository.save(Role.builder().name("ADMIN").build());
+//        Role customerRole = roleRepository.save(Role.builder().name("CUSTOMER").build());
+//        Role userRole = roleRepository.save(Role.builder().name("USER").build());
+//
+//        adminRole.setAuthorities(Set.of(createBeer, readBeer, updateBeer, deleteBeer));
+//        customerRole.setAuthorities(Set.of(readBeer));
+//        userRole.setAuthorities(Set.of(readBeer));
 
-        roleRepository.saveAll(List.of(adminRole, customerRole, userRole));
+//        roleRepository.saveAll(Arrays.asList(adminRole, customerRole, userRole));
+//        adminRole = roleRepository.save(adminRole);
+//        customerRole = roleRepository.save(customerRole);
+//        userRole = roleRepository.save(userRole);
 
         User user1 = User.builder()
                 .username("art")
@@ -78,10 +85,10 @@ public class UserDataLoader implements CommandLineRunner {
                 .role(customerRole)
                 .build();
 
-        userRepository.save(user1);
-        userRepository.save(user2);
-        userRepository.save(user3);
-//        userRepository.saveAll(List.of(user1, user2, user3));
+//        userRepository.save(user1);
+//        userRepository.save(user2);
+//        userRepository.save(user3);
+        userRepository.saveAll(List.of(user1, user2, user3));
 
         log.debug("Users loaded: {}", userRepository.count());
     }
