a1cdba54ee79e8604a37d77a30215836d1c22239
Art <d.art.shishkin@gmail.com>
2020-10-31 18:56:02 +0200

    104. Locking User Account After Failed Attempts

--- src/main/java/com/artarkatesoft/securitystudy/repositories/security/LoginFailureRepository.java
+++ src/main/java/com/artarkatesoft/securitystudy/repositories/security/LoginFailureRepository.java
@@ -1,7 +1,11 @@
 package com.artarkatesoft.securitystudy.repositories.security;
 
 import com.artarkatesoft.securitystudy.domain.security.LoginFailure;
+import com.artarkatesoft.securitystudy.domain.security.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.sql.Timestamp;
+
 public interface LoginFailureRepository extends JpaRepository<LoginFailure, Integer> {
+    long countAllByUserAndCreatedDateIsAfter(User user, Timestamp timestamp);
 }
--- src/main/java/com/artarkatesoft/securitystudy/security/listeners/AuthenticationFailureListener.java
+++ src/main/java/com/artarkatesoft/securitystudy/security/listeners/AuthenticationFailureListener.java
@@ -1,15 +1,20 @@
 package com.artarkatesoft.securitystudy.security.listeners;
 
 import com.artarkatesoft.securitystudy.domain.security.LoginFailure;
+import com.artarkatesoft.securitystudy.domain.security.User;
 import com.artarkatesoft.securitystudy.repositories.security.LoginFailureRepository;
 import com.artarkatesoft.securitystudy.repositories.security.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 @Slf4j
 @Component
@@ -18,6 +23,9 @@ public class AuthenticationFailureListener {
 
     private final LoginFailureRepository loginFailureRepository;
     private final UserRepository userRepository;
+
+    @Value("${app.security.max-attempts-to-lock-user:5}")
+    private Integer maxAttemptsToLockUser;
 
     @EventListener
     public void listen(AuthenticationFailureBadCredentialsEvent event) {
@@ -45,7 +53,20 @@ public class AuthenticationFailureListener {
             }
             LoginFailure loginFailure = loginFailureRepository.save(loginFailureBuilder.build());
             builder.append("\nLogin Failure saved with id: ").append(loginFailure.getId());
+
+            if (loginFailure.getUser() != null)
+                lockoutUser(loginFailure.getUser());
         }
         log.debug(builder.toString());
+    }
+
+    private void lockoutUser(User user) {
+        long failures = loginFailureRepository.countAllByUserAndCreatedDateIsAfter(user,
+                Timestamp.valueOf(LocalDateTime.now().minusDays(1)));
+        if (failures >= maxAttemptsToLockUser) {
+            user.setAccountNonLocked(false);
+            userRepository.save(user);
+            log.debug("User with id {} is locked out after {} failure attempts", user.getId(), failures);
+        }
     }
 }
