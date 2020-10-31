cc2d73a530d25fad96a08ca68664a7b27d11155f
Art <d.art.shishkin@gmail.com>
2020-10-31 16:16:20 +0200

    102. Logging of Authentication Success Events

--- src/main/java/com/artarkatesoft/securitystudy/security/listeners/AuthenticationSuccessListener.java
+++ src/main/java/com/artarkatesoft/securitystudy/security/listeners/AuthenticationSuccessListener.java
@@ -1,9 +1,15 @@
 package com.artarkatesoft.securitystudy.security.listeners;
 
+import com.artarkatesoft.securitystudy.domain.security.Role;
+import com.artarkatesoft.securitystudy.domain.security.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
@@ -11,6 +17,23 @@ public class AuthenticationSuccessListener {
 
     @EventListener
     public void listen(AuthenticationSuccessEvent event) {
-        log.debug("User Logged In Okay {}", event);
+        StringBuilder builder = new StringBuilder("User Logged In Okay");
+//        User user = (User) event.getAuthentication().getPrincipal();
+        if (event.getSource() instanceof UsernamePasswordAuthenticationToken) {
+            UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) event.getSource();
+
+            if (token.getPrincipal() instanceof User) {
+                User user = (User) token.getPrincipal();
+                builder.append("\nUsername is ").append(user.getUsername());
+                builder.append("\nUser ROLEs are ");
+                builder.append(user.getRoles().stream().map(Role::getName).collect(Collectors.joining(",")));
+            }
+            if (token.getDetails() instanceof WebAuthenticationDetails) {
+                WebAuthenticationDetails webAuthenticationDetails = (WebAuthenticationDetails) token.getDetails();
+                builder.append("\nRemote IP: ").append(webAuthenticationDetails.getRemoteAddress());
+                builder.append("\nSession ID: ").append(webAuthenticationDetails.getSessionId());
+            }
+        }
+        log.debug(builder.toString());
     }
 }
