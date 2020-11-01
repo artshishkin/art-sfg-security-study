35ebca4f382101ab9bd0b3ba79bb5aebe0eb6100
Art <d.art.shishkin@gmail.com>
2020-11-01 02:08:51 +0200

    Externalizing Lock Up properties

--- src/main/java/com/artarkatesoft/securitystudy/security/UserUnlockService.java
+++ src/main/java/com/artarkatesoft/securitystudy/security/UserUnlockService.java
@@ -4,10 +4,12 @@ import com.artarkatesoft.securitystudy.domain.security.User;
 import com.artarkatesoft.securitystudy.repositories.security.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -18,11 +20,14 @@ public class UserUnlockService {
 
     private final UserRepository userRepository;
 
-    @Scheduled(fixedRateString = "PT5S")
+    @Value("${app.security.lockup-accounts-duration}")
+    private Duration lockupAccountsDuration;
+
+    @Scheduled(fixedRateString = "${app.security.unlock-accounts-rate}")
     public void unlockUsers() {
         log.debug("Unlocking users");
         List<User> lockedUsers = userRepository.findAllByAccountNonLockedIsFalseAndLastModifiedDateBefore(
-                Timestamp.valueOf(LocalDateTime.now().minusSeconds(30)));
+                Timestamp.valueOf(LocalDateTime.now().minus(lockupAccountsDuration)));
 
         if (lockedUsers.size() > 0) {
             log.debug("Locked users found. Unlocking...");
--- src/test/java/com/artarkatesoft/securitystudy/web/controllers/BaseIT.java
+++ src/test/java/com/artarkatesoft/securitystudy/web/controllers/BaseIT.java
@@ -3,10 +3,14 @@ package com.artarkatesoft.securitystudy.web.controllers;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+@TestPropertySource(properties = {
+        "app.security.max-attempts-to-lock-user=300"
+})
 public abstract class BaseIT {
 
     @Autowired
