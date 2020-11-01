b9867b87313c7b3e2dd39b02fe0b1a7612e38b34
Art <d.art.shishkin@gmail.com>
2020-11-01 22:58:11 +0200

    118. Exclude Static Resources from Filter

--- src/main/java/com/artarkatesoft/securitystudy/security/google/Google2faFilter.java
+++ src/main/java/com/artarkatesoft/securitystudy/security/google/Google2faFilter.java
@@ -2,10 +2,14 @@ package com.artarkatesoft.securitystudy.security.google;
 
 import com.artarkatesoft.securitystudy.domain.security.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.boot.autoconfigure.security.servlet.StaticResourceRequest;
 import org.springframework.security.authentication.AuthenticationTrustResolver;
 import org.springframework.security.authentication.AuthenticationTrustResolverImpl;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
 
@@ -24,10 +28,19 @@ public class Google2faFilter extends GenericFilterBean {
     private final AuthenticationTrustResolver authenticationTrustResolver = new AuthenticationTrustResolverImpl();
     private final Google2faFailureHandler google2faFailureHandler = new Google2faFailureHandler();
 
+    private final RequestMatcher urlIs2fa = new AntPathRequestMatcher("/user/verify2fa");
+    private final RequestMatcher urlResource = new AntPathRequestMatcher("/resources/**");
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse resp = (HttpServletResponse) response;
+
+        StaticResourceRequest.StaticResourceRequestMatcher staticResourceRequestMatcher = PathRequest.toStaticResources().atCommonLocations();
+        if (urlIs2fa.matches(req) || urlResource.matches(req) ||
+                staticResourceRequestMatcher.matcher(req).isMatch()) {
+            chain.doFilter(request, response);
+        }
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
@@ -44,11 +57,8 @@ public class Google2faFilter extends GenericFilterBean {
                     google2faFailureHandler.onAuthenticationFailure(req, resp, null);
                     return;
                 }
-
-
             }
         }
-
         chain.doFilter(req, resp);
     }
 }
